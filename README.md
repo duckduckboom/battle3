93c8184d35f8ff54e3556e9f558b62db8f306ae7
Steve Jones <steve.jones763@gmail.com>
2021-10-05 16:03:17 +0100

    Added Sinatra and configured rspec.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # battle
+
+Calculator App
+
+- Page with a form
+
+-Sinatra
+-Capybara
+-Rspec
--- app.rb
+++ app.rb
@@ -1,21 +1,10 @@
 require 'sinatra'
 require "sinatra/reloader" if development?
 
-get '/' do
-    'Hello world!'
-end
+class Battle < Sinatra::Base
+    
+    get '/' do
+        erb :index
+    end
 
-get '/cat-form' do
-    erb(:cat_form)
-end
-
-post '/named-cat' do
-    p params
-    @name = params[:name]
-    erb(:index)
-end
-
-get '/random-cat' do
-    @name = ["Amigo", "Misty", "Almond"].sample
-    erb(:index)
 end
